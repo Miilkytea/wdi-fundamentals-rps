5c72e9f790e35c646cd6abffb4ef82814ea21fc7
Tara Milch <taramilch@gmail.com>
2015-06-19 01:32:43 -0700

    3.6 unit 3 HW added code for functions getPlayerMove and getComputerMove.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -23,20 +23,25 @@ function randomPlay() {
 
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
+    var playerMove = move || getInput();
     // If a `move` has a value, your expression should evaluate to that value.
+    
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return playerMove;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
+    var computerMove = move || randomPlay();
+    return computerMove;
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
     return /* Your Expression */;
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
+    var winner = ;
+
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
